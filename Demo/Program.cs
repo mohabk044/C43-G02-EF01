8cc3568cea74de542c61764da919cfb0546316ea
Mohab Khaled <mohabk044@gmail.com>
2025-02-03 19:09:48 +0200

    Q2 - Get all but the first 2 orders from customers in Washington.

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -6,7 +6,15 @@ namespace Assignment
         static void Main(string[] args)
         {
             #region Q1 - Get the first 3 orders from customers in Washington
-            var Result = CustomerList.Where(C => C.City == "Washington").Take(3);
+            //var Result = CustomerList.Where(C=> C.City == "Washington").Take(3);
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
+            #region Q2 - Get all but the first 2 orders from customers in Washington.
+            var Result = CustomerList.Where(C => C.City == "Washington").Skip(2);
             foreach (var item in Result)
             {
                 Console.WriteLine(item);
