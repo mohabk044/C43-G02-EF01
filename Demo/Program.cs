42de4f6cff6a7078afc2c8f375e0e0dc57e8c11e
Mohab Khaled <mohabk044@gmail.com>
2025-02-03 19:12:30 +0200

    Q4- Get the elements of the array starting from the first element divisible by 3.

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -24,8 +24,17 @@ namespace Assignment
             #endregion
 
             #region Q3- Return elements starting from the beginning of the array until a number is hit that is less than its position in the array.
+            //int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            //var Result = numbers.Where((n, i) => n < i).Skip(6);
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
+            #region Q4- Get the elements of the array starting from the first element divisible by 3.
             int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
-            var Result = numbers.Where((n, i) => n < i).Skip(6);
+            var Result = numbers.Where(n => n % 3 == 0).Take(4);
             foreach (var item in Result)
             {
                 Console.WriteLine(item);
