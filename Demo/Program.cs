e35d089679f95b8b5773cf5a2dc8d83266ebf69a
Mohab Khaled <mohabk044@gmail.com>
2025-02-03 19:01:00 +0200

    Transformationo Operators[Zipping]

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -5,23 +5,35 @@ namespace Demo
     {
         static void Main(string[] args)
         {
-            #region Video 01 
-            // 1- Any
-            var Result = ProductList.Any();
-            Console.WriteLine(Result);
-            var Result2 = ProductList.Any(P => P.UnitsInStock == 0);
-            Console.WriteLine(Result2);
-            // 2- All
-            var Result3 = ProductList.All(P => P.UnitsInStock == 0);
-            Console.WriteLine(Result3);
-            // 3- Contains
-            var Result4 = ProductList.Contains(ProductList[0]);
-            Console.WriteLine(Result4);
-            // 4- SequenceEqual
-            var Seq01 = Enumerable.Range(0, 100);
-            var Seq02 = Enumerable.Range(0, 100);
-            var Result5 = Seq01.SequenceEqual(Seq02);
-            Console.WriteLine(Result5);
+            #region Video 01 - Quantifier Operators
+            //// 1- Any
+            //var Result = ProductList.Any();
+            //Console.WriteLine(Result);
+            //var Result2 = ProductList.Any(P=>P.UnitsInStock==0);
+            //Console.WriteLine(Result2);
+            //// 2- All
+            //var Result3 = ProductList.All(P => P.UnitsInStock==0);
+            //Console.WriteLine(Result3);
+            //// 3- Contains
+            //var Result4 = ProductList.Contains(ProductList[0]);
+            //Console.WriteLine(Result4);
+            //// 4- SequenceEqual
+            //var Seq01 = Enumerable.Range(0, 100);
+            //var Seq02 = Enumerable.Range(0, 100);
+            //var Result5 = Seq01.SequenceEqual(Seq02);
+            //Console.WriteLine(Result5);
+            #endregion
+
+            #region Video 02 - Transformationo Operators[Zipping]
+            List<string> Words = new List<string>() { "ten", "twenty", "Thirty", "Fourty" };
+            int[] Num = { 10, 20, 30, 40, 50, 60 };
+            var result01 = Num.Zip(Words); //First Overload
+            var result02 = Num.Zip(Words, (Num, Words) => $"{Num}= {Words}"); //Third Overload
+            var result03 = Num.Zip(Words, [1, 2, 3]); //Second Overload
+            foreach (var item in result03)
+            {
+                Console.WriteLine(item);
+            }
             #endregion
         }
     }
