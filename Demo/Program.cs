f9243c99231f5fe546b05753402e6ac6d4bd0b0d
Mohab Khaled <mohabk044@gmail.com>
2025-02-03 19:08:48 +0200

    Q1 - Get the first 3 orders from customers in Washington

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Demo
+﻿using static Assignment.ListGenerators;
+namespace Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1 - Get the first 3 orders from customers in Washington
+            var Result = CustomerList.Where(C => C.City == "Washington").Take(3);
+            foreach (var item in Result)
+            {
+                Console.WriteLine(item);
+            }
+            #endregion
         }
     }
 }
